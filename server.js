7ad754cbfb5979ee85f35adca9dcda31c2623291
aryanpachchigar <aryanpachchigar12@gmail.com>
2025-01-13 22:53:44 +0530

    contact form

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 app.get('/', (req, res) => {
     res.send('Server is running!');
